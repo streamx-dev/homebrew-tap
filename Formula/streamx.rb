f5eac17ac9ccdb73452bdff444e814a7d934def1
jreleaserbot <jreleaser@kordamp.org>
2024-02-24 13:23:26 +0100

    streamx 0.0.1-alpha2

--- Formula/streamx.rb
+++ Formula/streamx.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.10.0 at 2024-02-24T13:21:27.756665+01:00
+# Generated with JReleaser 1.10.0 at 2024-02-24T13:23:15.68177+01:00
 
 class Streamx < Formula
   desc "CLI for StreamX"
   homepage "https://www.streamx.dev"
-  url "https://github.com/streamx-dev/streamx-cli/releases/download/0.0.1-alpha1/streamx-0.0.1-alpha1.zip"
-  version "0.0.1-alpha1"
-  sha256 "d966b0e60564bcb4db306f1f40b2cd367f398fc4aeebcb8bcb8ef62d883c3649"
+  url "https://github.com/streamx-dev/streamx-cli/releases/download/0.0.1-alpha2/streamx-0.0.1-alpha2.zip"
+  version "0.0.1-alpha2"
+  sha256 "d4becabdbc73a4ecbf0ba1f132faf997d1752dbb4c33d737d9d9a280f127752f"
   license "EULA"
 
 
@@ -16,6 +16,6 @@ class Streamx < Formula
 
   test do
     output = shell_output("#{bin}/streamx --version")
-    assert_match "0.0.1-alpha1", output
+    assert_match "0.0.1-alpha2", output
   end
 end
