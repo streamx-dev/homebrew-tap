839ca418718a82ec12392ccb7d2be32380c76802
jgardo-ds <github.actions@streamx.dev>
2024-11-07 10:40:43 +0000

    Update homebrew formulas.

---
+++ Formula/streamx@0.2.3.rb
@@ -0,0 +1,21 @@
+# Generated with JReleaser 1.10.0 at 2024-04-02T10:36:44.740067+02:00
+
+class StreamxAT023 < Formula
+  desc "CLI for StreamX"
+  homepage "https://www.streamx.dev"
+  url "https://github.com/streamx-dev/streamx-cli/releases/download/0.2.3/streamx-0.2.3.zip"
+  version "0.2.3"
+  sha256 "d1a5a87c6b2f3bfefd9582361341bf71687973fa5dd754a90d11f8ee4611e5e5"
+  license "EULA"
+
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/streamx" => "streamx"
+  end
+
+  test do
+    output = shell_output("#{bin}/streamx --version")
+    assert_match "0.2.3", output
+  end
+end
---
+++ Formula/streamx@0.3.1.rb
@@ -0,0 +1,21 @@
+# Generated with JReleaser 1.12.0 at 2024-05-29T10:24:19.172967+02:00
+
+class StreamxAT031 < Formula
+  desc "CLI for StreamX"
+  homepage "https://www.streamx.dev"
+  url "https://github.com/streamx-dev/streamx-cli/releases/download/0.3.1/streamx-0.3.1.zip"
+  version "0.3.1"
+  sha256 "6931fb56c7eb7acad1048ea17d2dab522bd33932caffd9c8526cc21c1a2c987d"
+  license "EULA"
+
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/streamx" => "streamx"
+  end
+
+  test do
+    output = shell_output("#{bin}/streamx --version")
+    assert_match "0.3.1", output
+  end
+end
