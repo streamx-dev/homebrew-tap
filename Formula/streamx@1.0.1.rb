3c10122b1fad055b28bfed5d5fb2f50cac3171b6
hamster-bay <github.actions@streamx.dev>
2024-12-24 09:27:34 +0000

    Update homebrew formulas.

---
+++ Formula/streamx@1.0.1.rb
@@ -0,0 +1,21 @@
+# Generated with JReleaser 1.15.0 at 2024-12-24T09:27:13.972078914Z
+
+class StreamxAT101 < Formula
+  desc "CLI for StreamX"
+  homepage "https://www.streamx.dev"
+  url "https://github.com/streamx-dev/streamx-cli/releases/download/1.0.1/streamx-1.0.1.zip"
+  version "1.0.1"
+  sha256 "d175fb24b1010af6f58a3bfd907beed48ab596660f75750a5329779769e50606"
+  license "EULA"
+
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/streamx" => "streamx"
+  end
+
+  test do
+    output = shell_output("#{bin}/streamx --version")
+    assert_match "1.0.1", output
+  end
+end
